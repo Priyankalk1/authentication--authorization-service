d03b246966311a305fbea998e6a31bcd4ca0fc16
Priyankalk1 <Priyankalk@maveric-systems.com>
2022-08-22 22:10:59 +0530

    Initial commit

---
+++ src/main/java/com/maveric/authenticationauthorizationservice/AuthenticationAuthorizationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.maveric.authenticationauthorizationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationAuthorizationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationAuthorizationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maveric/authenticationauthorizationservice/AuthenticationAuthorizationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maveric.authenticationauthorizationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationAuthorizationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
